cbe498d0c20b49c651de70ce218dcef2a2ab758e
Kate Ray <email@kateray.net>
2012-01-21 12:34:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tytc::Application.config.secret_token = '218287b2b6b2f102bb08eaa6a4b40b49f7d54d3b2520d8e85dcfe20be5b4f9f440ce9ecea91e0dc3eebec8d7988afa8c65c02ec0d63c7b461f8a66cfec945bde'
