3e8ca57bad8120c3a9ea5ad2d0898205c3654a4a
Kate Ray <email@kateray.net>
2013-12-27 11:05:10 -0800

    added assets precompile false to application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Tytc
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.assets.initialize_on_precompile = false
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
